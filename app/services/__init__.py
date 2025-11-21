264a19890ff127ecd342525616f7f22cf5396424
Nagaraj <snagarajseeni@gmail.com>
2025-11-21 15:14:45 +0530

    removed unused code

--- app/services/__init__.py
+++ app/services/__init__.py
@@ -1,19 +1,7 @@
 """Services package for Photo Proof Studio API."""
 
-from .analytics_service import AnalyticsService
-from .layout_service import LayoutService
 from .invoice_service import InvoiceService
-from .notification_service import NotificationService
-from .workflow_service import WorkflowService
-from .delivery_service import DeliveryService
-from .ui_customization_service import UICustomizationService
 
 __all__ = [
-    "AnalyticsService",
-    "LayoutService", 
     "InvoiceService",
-    "NotificationService",
-    "WorkflowService",
-    "DeliveryService",
-    "UICustomizationService"
 ]
